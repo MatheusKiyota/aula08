8dc44846ca12b3cb0ebe56f7efca1638011664e9
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-12-09 17:32:11 -0400

    atualizaçao

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,50 +1,42 @@
 import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 export default function Registrar() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
 
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
 
-  async function RegistrarMarcelino(event) {
+  const navigation = useNavigate();
+
+  const registrar = async(event) => {
     event.preventDefault();
-
-    try {
-      await fetch("http://localhost:3000/usuarios", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+    try{
+      const resposta = await fetch('http://localhost:3000/usuarios', {
+        method: 'POST',
+        headers:{'Content-Type': 'Application/json'},
         body: JSON.stringify({
           nome: nome,
-          email: email,
-        }),
+          email: email
+        })
       });
-      alert("Usuário registrado com sucesso!"); 
-    } catch (error) {
-      alert("Ocorreu um erro na aplicação."); 
+      if(resposta.ok){
+        navigation('/');
+      }
+    } catch (error){
+      console.log(error)
+      alert('Ocorreu um erro na aplicação: ');
     }
   }
 
+
   return (
     <main>
-      <form onSubmit={RegistrarMarcelino}>
-   
-        <input
-          type="text"
-          placeholder="Digite seu nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-
-
-        <input
-          type="email"
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-   
-        <button type="submit">Registrar</button>
+      <form onSubmit={registrar}>
+        <input type="text" name="" id="" value={nome}  onChange={(event) => setNome(event.target.value)}/> <br/>
+        <input type="email" email="" id="" onChange={(event) => setEmail(event.target.value)}/> <br/>
+        <button>Salvar</button>
       </form>
     </main>
+        
   );
 }
